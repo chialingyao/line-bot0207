dec6f1ffbb3264eecc44ed2acc9c17729b927779
Lillian <inureye1@gmail.com>
2019-02-08 14:35:42 +0800

    first commit

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('r7sj2S01EoafYqsWV7aPx3KN/8Pb312Va+F8vZffoqI85s+c36il7ysLASE5vCFfEdK8gtPsrvclgnSalaeei3XEQHPD6dPZPO+1RDWplp0oP9WzfiV2YeYcmt2VIQGVSfbkRj+bZcDrFCHv3F1VNQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('767d08776cff00b556562d7660ea0652')
+line_bot_api = LineBotApi('iAJNjyrOPPm1ahAobtCY8SClSbekUSvOAPiXQ+4um3QXn90wUOZ8CmIas/mqrBL7lF4KvaHR7TzuZoVIa/35Yd2pzd4p8v6NJo40QvayqnwOcBCqi9l04eSEuDWd9TX+HeILSiA2yNAKL+HkTzzRAgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('aaff4eff258de58ae6d92f4d497b8e47')
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,10 +37,31 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃飯ㄌㄇ'
+    r = '很抱歉，您說什麼'
+
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+        return
+
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位，是嗎'
+   
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
